19a0358b1bbdd3c1d9745e2a425119c39792e6a0
AlazarBekele <alazarthe70@gmail.com>
2025-01-13 21:43:17 +0300

    install pillow, create form, deploy model post

--- Scraping/models.py
+++ Scraping/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Post (models.Model):
+
+    Title = models.CharField (max_length=40)
+    Bio = models.TextField ()
+    Image = models.ImageField (upload_to='Post_Picture')
+
+    def __str__(self):
+        return self.Title
