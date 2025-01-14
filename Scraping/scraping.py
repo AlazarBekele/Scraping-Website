3035db5e30bb19b814a3f32cb0afbf05f016540e
AlazarBekele <alazarthe70@gmail.com>
2025-01-14 19:48:00 +0300

    fix image error

---
+++ Scraping/scraping.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrap_data (url):
+
+    try:
+        respose = requests.get(url)
+        if respose.status_code == 200:
+
+            soup = BeautifulSoup(respose.content, 'body.html')
+            data = soup.find('title').text
+            return data
+        
+        else:
+
+            return "Failed to fetch data: Invalid status code"
+        
+    except Exception as e:
+        
+        return f"Error: {str(e)}"
