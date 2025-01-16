83af2f305d566ed2e9c49c826ca8b0af5de520d4
AlazarBekele <alazarthe70@gmail.com>
2025-01-16 21:17:08 +0300

    Select out the tags

--- Scraping/scraping.py
+++ Scraping/scraping.py
@@ -2,8 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = "https://www.ebay.com/sch/i.html?_nkw=macbook+air+m2&_sacat=0&_from=R40&_trksid=p4432023.m570.l1311"
+url = "https://webscraper.io/test-sites/e-commerce/static/computers/laptops"
 
 Check = requests.get(url)
 
-print (Check.status_code)
+soup = BeautifulSoup(Check.text, "html")
+# print (Check.status_code)
+print (soup.div)
