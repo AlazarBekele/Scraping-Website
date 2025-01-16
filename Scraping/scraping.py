62b1bc129f82b390df1c47deacc1f5ea1a463900
AlazarBekele <alazarthe70@gmail.com>
2025-01-16 20:40:10 +0300

    Check code status

--- Scraping/scraping.py
+++ Scraping/scraping.py
@@ -1,30 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-from telegram import Bot
 
+url = "https://www.ebay.com/sch/i.html?_nkw=macbook+air+m2&_sacat=0&_from=R40&_trksid=p4432023.m570.l1311"
 
-def scrap_data (url):
+Check = requests.get(url)
 
-    try:
-        respose = requests.get(url)
-        if respose.status_code == 200:
-
-            soup = BeautifulSoup(respose.content, 'body.html')
-            data = soup.find('title').text
-            return data
-        
-        else:
-
-            return "Failed to fetch data: Invalid status code"
-        
-    except Exception as e:
-        
-        return f"Error: {str(e)}"
-    
-
-TELEGRAM_BOT_TOKEN = '7625611051:AAGorAAExqg3t1DxAa25edR_2DvRt3p0Fts'
-
-    
-
-# def send_to_telegram (message):
+print (Check.status_code)
---
+++ importantError.py
@@ -0,0 +1,28 @@
+
+from telegram import Bot
+
+
+def scrap_data (url):
+
+    try:
+        respose = requests.get(url)
+        if respose.status_code == 200:
+
+            soup = BeautifulSoup(respose.content, 'body.html')
+            data = soup.find('title').text
+            return data
+        
+        else:
+
+            return "Failed to fetch data: Invalid status code"
+        
+    except Exception as e:
+        
+        return f"Error: {str(e)}"
+    
+
+TELEGRAM_BOT_TOKEN = '7625611051:AAGorAAExqg3t1DxAa25edR_2DvRt3p0Fts'
+
+    
+
+# def send_to_telegram (message):
